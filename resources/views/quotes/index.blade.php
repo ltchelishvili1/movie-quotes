0a819319ce3e1dcaa9eaa84d12a8719d142e6030
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-30 12:09:19 +0400

    feat: create homepage view

---
+++ resources/views/quotes/index.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <div class="flex flex-col items-center">
+        <img class="h-96 rounded-lg" src='https://nostal.ge/wp-content/uploads/2018/07/jariskacis-mama-.png' />
+        <p class="text-5xl pt-16 text-white">“What should I tell you your mother?!”</p>
+        <a class="text-5xl pt-28 text-white underline" href="#">The Son of Soldier</a>
+    </div>
+</x-layout>
