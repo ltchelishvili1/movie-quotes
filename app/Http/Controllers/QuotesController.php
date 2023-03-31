2f85b24fcab45a9f24c2332e5e42cd20b57c2c2b
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-31 12:14:50 +0400

    chore: refactor

--- app/Http/Controllers/QuotesController.php
+++ app/Http/Controllers/QuotesController.php
@@ -9,9 +9,9 @@ class QuotesController extends Controller
 	public function index(Movie $movie)
 	{
 		$randomMovie = $movie->get()->random();
-		return view('quotes.index',[
+		return view('quotes.index', [
 			'movie' => $randomMovie,
-			'quote' => $randomMovie->quote->random()
+			'quote' => $randomMovie->quote->random(),
 		]);
 	}
 }
