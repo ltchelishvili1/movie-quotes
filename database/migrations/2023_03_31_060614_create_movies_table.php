8b17746d4c08bdf8defa457f47a49a7fef6c2f95
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-03-31 10:25:02 +0400

    feat: add database models migration

--- database/migrations/2023_03_31_060614_create_movies_table.php
+++ database/migrations/2023_03_31_060614_create_movies_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+          Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('slug')-> unique();
+            $table->string('thumbnail')->nullable();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('movies');
     }
 };
