929f3bbc73f74bb4ec328165aa0ae7fdb2d681fd
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-01 18:05:42 +0400

    feat: add artisan command to create users remove redundant tables

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create {email : The email address of the user} {password : The password for the user}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+        $user = new User();
+        $user->email = $email;
+        $user->password = bcrypt($password);
+        $user->save();
+        $this->info('User created successfully!');
+    }
+}
