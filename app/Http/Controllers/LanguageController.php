862f2a6aae0723d7b191f91836ca62e49a88aac0
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-02 02:48:04 +0400

    refactor: add return type

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Session;
 
 class LanguageController extends Controller
 {
-	public function changeLang($lang)
+	public function changeLang($lang): RedirectResponse
 	{
 		if (array_key_exists($lang, Config::get('languages')))
 		{
