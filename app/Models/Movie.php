b5a1488552e24d2fee74fb66b5c98c58abd27f78
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-04-05 10:37:43 +0400

    feat: readme optimize relationships

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Translatable\HasTranslations;
 
@@ -17,11 +16,6 @@ class Movie extends Model
 	public $translatable = ['name'];
 
 	protected $guarded = ['id'];
-
-	public function user(): BelongsTo
-	{
-		return $this->belongsTo(User::class);
-	}
 
 	public function quotes(): HasMany
 	{
